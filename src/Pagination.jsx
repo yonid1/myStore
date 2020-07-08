1d8ed57a31f887b0307977eafc3a05355bd87386
yonid1 <yontand@gmail.com>
2020-07-08 21:53:40 +0300

    commit amend

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 export default function Pagination({
   productPerPage,
   totalProduct,
   currentPage,
   list,
+  setList,
   setCurrentPage,
-  
 }) {
   const indexOfLastProduct = currentPage * productPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productPerPage;
-  const currentPosts = list.slice(indexOfFirstProduct, indexOfLastProduct);
+  const newList = list.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  useCallback(() => {
+    setList(newList);
+  }, [currentPage]);
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
@@ -19,13 +23,22 @@ export default function Pagination({
     pageNumbers.push(i);
   }
 
-  return <nav>
+  return (
+    <nav>
       <ul>
-          {pageNumbers.map(number =>(
-              <li key = {number}>
-                  <a onClick = {paginate(number)} href ='!#'> {number} </a>
-              </li>
-          ))}
+        {pageNumbers.map((number) => (
+          <li key={number}>
+            <a
+              onClick={() => {
+                paginate(number);
+              }}
+              href="!#"
+            >
+              {number}
+            </a>
+          </li>
+        ))}
       </ul>
-  </nav>;
+    </nav>
+  );
 }
