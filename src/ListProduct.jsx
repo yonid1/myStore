2527473cc401b50719c5e1ce2cddfe680013e585
Yoel31 <yoel301@gmail.com>
2020-07-09 22:42:43 +0300

    Pagination good

--- src/ListProduct.jsx
+++ src/ListProduct.jsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
 import DeleteIcon from "@material-ui/icons/Delete";
 import IconButton from "@material-ui/core/IconButton";
 // import ListItemText from '@material-ui/core/ListItemText';
-import ListItem from '@material-ui/core/ListItem';
+import ListItem from "@material-ui/core/ListItem";
 import TextField from "@material-ui/core/TextField";
+import Pagination from "./Pagination";
 
 export default function ListProduct(props) {
+  // const [search, setSearch] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productPerPage] = useState(2);
+  const [listView, setListView] = useState(props.list);
+  const [listSearch, setListSearch] = useState(props.list);
 
-  
-  const newList = props.list.filter((cars) => {
-    return cars.car.toLowerCase().includes(props.search.toLowerCase());
-  })
-    // React.useEffect(()=>{
-    //     console.log("useEffect list");
-        
-    //     console.log(props.list);
-    // },[props.list])
+  useEffect(() => {
+    const newList = props.list.filter((cars) => {
+      return cars.car.toLowerCase().includes(props.search.toLowerCase());
+    });
+    setListSearch(newList);
+  }, [props.search]);
+
+  // React.useEffect(()=>{
+  //     console.log("useEffect list");
+
+  //     console.log(props.list);
+  // },[props.list])
   return (
-
     <div>
       <TextField
         label="Search"
@@ -31,26 +39,31 @@ export default function ListProduct(props) {
         onChange={(e) => props.setSearch(e.target.value)}
       />
 
-    <List>
-      {newList.map((item, index) => (
-          
-          <ListItem key={item.car}
-          >
-         <div >{item.car}</div> {item.price} {item.date} {item.discretion}
-          <IconButton
-            edge="end"
-            aria-label="delete"
-            size="small"
-            variant="outlined"
-            type="button"
-            onClick={() => props.RemoveProduct(index)}
-          >
-            <DeleteIcon />
-          </IconButton>
+      <List>
+        {listView.map((item, index) => (
+          <ListItem key={item.car}>
+            <div>{item.car}</div> {item.price} {item.date} {item.discretion}
+            <IconButton
+              edge="end"
+              aria-label="delete"
+              size="small"
+              variant="outlined"
+              type="button"
+              onClick={() => props.RemoveProduct(index)}
+            >
+              <DeleteIcon />
+            </IconButton>
           </ListItem>
-        
-      ))}
+        ))}
       </List>
+      <Pagination
+        setList={setListView}
+        list={listSearch}
+        productPerPage={productPerPage}
+        totalProduct={listSearch.length}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </div>
   );
 }
